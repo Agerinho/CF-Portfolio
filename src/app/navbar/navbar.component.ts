eb0f8fc5b0ad569ef5af732755d460feae6ddefb
Markus Ager <agermarkus123@gmail.com>
2022-06-27 00:37:08 +0200

    nav, about, contact

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,6 +10,22 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    const navToggle = document.getElementById("navToggle")
+    const navToggleIcon = document.getElementById("navToggleIcon")
+    const mobileNav = document.getElementById("navbarMobile")
+    const navLinks = document.querySelectorAll("#navbarMobile a")
 
+    if(navToggle) {
+      navToggle.addEventListener("click", () => {
+        mobileNav?.classList.toggle("active")
+        navToggleIcon?.classList.toggle("bi-x")
+      })
+      navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+          mobileNav?.classList.toggle("active")
+          navToggleIcon?.classList.toggle("bi-x")
+        })
+      })
+    }
+  }
 }
